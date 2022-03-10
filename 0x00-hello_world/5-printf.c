925c0cefc723bb3edea620e3c3ac9fa20c771854
ikechukwu-peter <pitiyke@gmail.com>
2022-03-10 12:36:10 -0800

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-/*
-*function name main 
-*Return -returns 0
-*/
+/**
+ * main - main function
+ * Description: The function print some text to the stdout
+ * Return: returns void
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
