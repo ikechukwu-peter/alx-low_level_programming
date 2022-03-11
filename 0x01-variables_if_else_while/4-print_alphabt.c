cb29f48346ec4c41b9b609be2392445f6bd9cb9b
ikechukwu-peter <pitiyke@gmail.com>
2022-03-11 08:33:44 -0800

    testing

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: prints all aphabet in lowercase except q and e
+ *Return: Always 0
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c >= 'z'; c++)
+{
+if (c != 'q' || c != 'e')
+{
+putchar(c);
+}
+}
+putchar('\n');
+return (0);
+}
