3174bd3d5af4c170133a5258d6151abef752b27f
ikechukwu-peter <pitiyke@gmail.com>
2022-03-11 07:17:17 -0800

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - entry point
+ *Description: converts string to lowercase
+ *Return: Alwasy 0 (success)
+ */
+int main(void)
+{
+char c;
+for(c='a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+return (0);
+}
