ea355e6f496c850e817f793264b0d75d05f21239
ikechukwu-peter <pitiyke@gmail.com>
2022-03-11 08:57:00 -0800

    testing

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: printing all the number of base 10
+ *Return: Always return 0
+ */
+int main(void)
+{
+int c;
+for (c = 0; c < 10; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
