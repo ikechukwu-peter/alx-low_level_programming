401e517f0bfbe8caee2d4ed8e9412c8c597dec7d
ikechukwu-peter <pitiyke@gmail.com>
2022-03-15 08:14:49 -0700

    make instructions clear

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 /**
  *main - entry point
  *Description: Prints _putchar to the terminal
  *Return: Alwasy 0
  */
-int main(void)
+void writechar()
 {
 printf("_putchar\n");
-return (0);
 }
 
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+ /**
+ *main - entry point
+ *Description: calls putchar function
+ *Return: Always 0
+ */
+int main(void)
+{
+writechar();
+return (0);
+}
