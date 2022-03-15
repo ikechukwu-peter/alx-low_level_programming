31016139913ac6b03fe02641e72467b1f5a4928b
ikechukwu-peter <pitiyke@gmail.com>
2022-03-15 10:51:14 -0700

    ff

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_islower - entry point
+ *@c: a letter
+ *
+ *Description: checks if a letter in in lowercase
+ *Return: either 1 or 0
+ */
+int _islower(int c)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+if (c == a)
+{
+return (1);
+};
+else
+{
+return (0);
+};
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,4 +3,4 @@
  *Description: display alphabet in lowercase
  *Return: Always 0
  */
-int print_alphabet_x10(void);
+int int _islower(int c);
