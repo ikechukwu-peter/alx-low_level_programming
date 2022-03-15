5383eb93debcf00db3162f394804a22a63af1186
ikechukwu-peter <pitiyke@gmail.com>
2022-03-15 13:30:57 -0700

    gf

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *times_table - entry point
+ *Description: checks if a letter is in uppercase
+ *Return: either 1 or 0
+ */
+void times_table(void)
+{
+int i, j;
+for (i = 9; i < 13; i++)
+{
+for (j = 0; j < 13; j++)
+{
+_putchar(i * j)
+}
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,6 @@
 /**
  *main - entry point
- *@c: an int
- *@d: an int
- *
  *Description: display alphabet in lowercase
  *Return: Always 0
  */
-int _abs(int c);
+void times_table(void);
