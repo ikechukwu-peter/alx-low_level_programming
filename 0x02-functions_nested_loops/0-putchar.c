8a41e7289316ebdee96e4ecd8a8a715fabccc0e9
ikechukwu-peter <pitiyke@gmail.com>
2022-03-15 08:34:11 -0700

    tired

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *main - entry point
+ *writechar - entry point
  *Description: Prints _putchar to the terminal
  *Return: Alwasy 0
  */
-int writechar()
+int writechar(void)
 {
 printf("_putchar\n");
 return (0);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,12 @@
- /**
- *main - entry point
- *Description: calls putchar function
- *Return: Always 0
- */
+/**
+*main - entry point
+*Description: calls putchar function
+*Return: Always 0
+*/
 int main(void)
 {
-int writechar();
+int writechar(void);
 writechar();
 return (0);
 }
+
