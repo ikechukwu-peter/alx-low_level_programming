36f9ca5450d3527b898b31bcdd48f4d17fdc2536
ikechukwu-peter <pitiyke@gmail.com>
2022-03-15 12:54:05 -0700

    recheck

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include "main.h"
+#include <unistd.h>
 /**
  *writechar - entry point
  *Description: Prints _putchar to the terminal
  *Return: Alwasy 0
  */
-int writechar(void)
+int writechar(char)
 {
-printf("_putchar\n");
+_putchar('-');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,6 @@
 /**
  *main - entry point
- *@c: any
  *Description: display alphabet in lowercase
  *Return: Always 0
  */
-int print_last_digit(int c);
-int _putchar(int c);
+int _putchar(char);
