ba8e97cd50181937921e356f725f80fc85398c9d
ikechukwu-peter <pitiyke@gmail.com>
2022-03-17 12:36:42 -0700

    feat: main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,7 @@
 /**
  *main - entry point
- *Description: check the code 
+ *Description: check the code
  *Return: Always 0
  */
-int main(void)
-{
-print_numbers();
-return (0);
-}
+int print_numbers(void);
+
