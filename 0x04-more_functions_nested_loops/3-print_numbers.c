84dfff5f16e373308b4070c03afe8022b94ab69b
ikechukwu-peter <pitiyke@gmail.com>
2022-03-17 12:26:31 -0700

    feat: print all numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_numbers - print all number from 0-9
+ *Description: print all natural numbers from 0-9
+ *Return: void
+ */
+void print_numbers(void)
+{
+int c;
+for (c = 0; c < 10; c++)
+{
+_putchar(c + '0');
+}
+_putchar('/n');
+}
