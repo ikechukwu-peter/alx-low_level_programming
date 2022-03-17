c62854923f29dab04cd1e87bb705088f9f93aec3
ikechukwu-peter <pitiyke@gmail.com>
2022-03-17 05:01:07 -0700

    initial commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - test function that prints negative or positive
+ *Return: 0
+ */
+int main(void)
+{
+int i;
+
+i = 98;
+positive_or_negative(i);
+
+return (0);
+}
